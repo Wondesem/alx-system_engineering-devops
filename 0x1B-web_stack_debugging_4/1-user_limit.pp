6a1ada79ab8dc0ba3211132c00a1252c9c59272c
wondesem <wseminew50gmail.com>
2023-03-08 23:00:02 -0800

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
-#Increases limit of open files
-exec {'hard-limit':
-    command  => 'sed -i "/holberton hard/s/5/10000" /etc/security/limits.conf',
-    provider => shell,
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec {'soft-limit':
-    command  => 'sed -i "/holberton hard/s/4/10000" /etc/security/limits.conf',
-    provider => shell,
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }

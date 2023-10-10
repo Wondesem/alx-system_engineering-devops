215045135429a2309903f4db036c5655875aae42
Wondesem <wseminew50@gmail.com>
2023-10-10 07:09:46 -0700

    minor correction tasl 1 and 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0/python"
+        "User-Agent": "Python/requests"
     }
     params = {
         "limit": 10

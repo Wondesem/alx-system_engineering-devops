51b04500ba224e4aeea2eea6d4d84a56bbcc6dbf
Wondesem <wseminew50@gmail.com>
2023-10-10 21:49:56 -0700

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-"""This module is in charge of making the connection with the api and prints
-    the titles of the first 10 hot posts listed for a given subreddit."""
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """ Queries to Reddit API """
+    u_agent = 'Python/requests'
+
     headers = {
-        "User-Agent": "Python/requests"
+        'User-Agent': u_agent
     }
+
     params = {
-        "limit": 10
+        'limit': 10
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
